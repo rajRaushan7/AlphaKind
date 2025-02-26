e6ac7ed930ffe6af39a4e7b9ccecb7db2881a21e
rajRaushan7 <raj1618192@gmail.com>
2025-02-26 23:16:23 +0530

    add subjects route and semWiseNotes route

--- backend/routes/allNotes.js
+++ backend/routes/allNotes.js
@@ -50,4 +50,53 @@ router.get('/fetchAllNotes', async (req, res) => {
     }
 });
 
+// Router 3: Fetch all Subjects of a particular semester using GET 'api/notes/subjects'
+
+router.get('/subjects', async (req, res) => {
+    try {
+        const { semester } = req.body;
+        // check if semester is a valid
+        if(!Number.isInteger(semester) || semester < 1 || semester > 8){
+            return res.status(400).send("Invalid semester request");
+        }
+        const notes = await Note.find({ semester: semester });
+        if (!notes.length){
+            return res.status(400).send("No notes of this semester is available");
+        }
+
+        // Fetch all subjects in the form of an array
+
+        // Use Set to store unique semester-subject pairs
+        const subjectsSet = new Set();
+        notes.forEach(note => subjectsSet.add(JSON.stringify([semester, note.subject])));
+
+        // Convert Set back to array and parse JSON strings
+        const subjects = [...subjectsSet].map(item => JSON.parse(item));
+        return res.json([...subjects]);
+
+    } catch(error) {
+        return res.status(500).send("Internal Server Error");
+    }
+});
+
+// Route 4: Fetch all semester wise notes using: GET 'api/notes/semWiseNotes'
+
+router.get('/semWiseNotes', async (req, res) => {
+    try {
+        const { semester, subject } = req.body;
+        if(!semester || !subject){
+            return res.status(400).send("Invalid credentials");
+        }
+
+        const note = await Note.find({ semester, subject });
+        if(!note.length){
+            return res.status(404).send("No notes of this subject/semester is available");
+        }
+        return res.json({ note });
+
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
+    }
+});
+
 module.exports = router;
